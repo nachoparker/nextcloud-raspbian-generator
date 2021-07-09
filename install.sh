bfefce43b1256ab4b0b1a33c6b1f6b7aef9a8e0b
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-07-09 12:45:56 +0200

    install.sh: Remove duplicate popd command

--- install.sh
+++ install.sh
@@ -63,8 +63,6 @@ install_app    ncp.sh
 run_app_unsafe bin/ncp/CONFIG/nc-init.sh
 bash /usr/local/bin/ncp-provisioning.sh
 
-popd
-
 git clone --depth 20 -b "$BRANCH" -q https://github.com/nextcloud/nextcloudpi.git "$TMPDIR/nextcloudpi-git" || {
   echo "No internet connectivity"
   exit 1
